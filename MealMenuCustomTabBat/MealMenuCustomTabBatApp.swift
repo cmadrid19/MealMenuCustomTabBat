5a7458456e077298116fa657241ef84ebbf01065
Max <maxim.macari.dam@gmail.com>
2020-11-13 16:00:02 +0100

    Initial Commit

---
+++ MealMenuCustomTabBat/MealMenuCustomTabBatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealMenuCustomTabBatApp.swift
+//  MealMenuCustomTabBat
+//
+//  Created by Maxim Macari on 13/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct MealMenuCustomTabBatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
